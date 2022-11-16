36759a43247309d115178554ce567d15683af2b6
naveh <navehle@gmail.com>
2022-11-16 14:16:00 +0200

    is book empty decorator

--- main.py
+++ main.py
@@ -3,18 +3,30 @@ def make_bold(fn):
         return "<b>" + fn() + "</b>"
     return wrapped
 
+
 def make_italic(fn):
     def wrapped():
         return "<i>" + fn() + "</i>"
     return wrapped
 
+
 def make_underline(fn):
     def wrapped():
         return "<u>" + fn() + "</u>"
     return wrapped
-@make_bold
-@make_italic
-@make_underline
+
+
+def if_books(fn):
+    def wrapped():
+        if books:
+            return fn()
+    return wrapped
+
+
+@if_books
 def hello():
     return "hello world"
+
+
+books = input("Insert book names:").split()
 print(hello())
